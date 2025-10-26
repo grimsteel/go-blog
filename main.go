7c7278e86855176b9ee76cdea004ff4b4fdcafcb
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2025-10-25 23:57:40 -0500

    initial website and index page

--- main.go
+++ main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
+
+// jetbrains
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// jetbrains
+func renderTemplate(data any, templateFile string, w *http.ResponseWriter) {
+	t, _ := template.ParseFiles(fmt.Sprintf("templates/%s.html", templateFile))
+	check(t.Execute(*w, data))
+}
+
+type Post struct {
+	Id string
+	Date string
+	Filename string
+	Title string
+}
+
+func getPostList() ([]Post) {
+	postListJson, err := os.ReadFile("posts.json")
+	check(err)
+
+	// parse JSON
+	var posts []Post
+	check(json.Unmarshal(postListJson, &posts))
+
+	return posts
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	posts := getPostList()
+	
+	renderTemplate(posts, "index", &w)
+}
 
 func main() {
 	listen_address := os.Getenv("LISTEN_ADDRESS")
@@ -28,5 +67,5 @@ func main() {
 
 	// start the server
 	fmt.Printf("Listening on %s\n", listen_address)
-  server.ListenAndServe()
+  check(server.ListenAndServe())
 }
